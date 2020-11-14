6ac7a84c77ed3480db6c8618ce365af07fbb95a5
ajitku425 <ajitkumar536@gmail.com>
2020-11-14 20:41:01 +0530

    update main springboot class

--- src/main/java/io/controller/controllerexampleapp/ControllerExampleAppApplication.java
+++ src/main/java/io/controller/controllerexampleapp/ControllerExampleAppApplication.java
@@ -2,9 +2,16 @@ package io.controller.controllerexampleapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ControllerExampleAppApplication {
+public class ControllerExampleAppApplication extends SpringBootServletInitializer {
+	
+	@Override
+	 protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	  return application.sources(ControllerExampleAppApplication.class);
+	 }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ControllerExampleAppApplication.class, args);
